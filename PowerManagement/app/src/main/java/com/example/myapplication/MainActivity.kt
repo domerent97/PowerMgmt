dbc6ef18e233a7c7706e694fd0bf226ea8a59470
Doménica Rentería Berrospe <ladomerents@MacBook-Pro-de-Domenica-2.local>
2020-04-03 14:51:38 -0600

    PowerMgmt en Kotlin

---
+++ PowerManagement/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val myContext = applicationContext
+        setContentView(R.layout.activity_main)
+        batteryTxt = findViewById(R.id.batteryTxt) as TextView
+        powerStatus = findViewById(R.id.powerStatus) as TextView
+        this.registerReceiver(this.mBatInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        this.registerReceiver(this.plugInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+    }
+
+    private var powerStatus: TextView? = null
+
+    private val plugInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(ctxt: Context, intent: Intent) {
+            val plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+            if (plugged == BatteryManager.BATTERY_PLUGGED_AC) powerStatus!!.text =
+                "Plugged to AC" else if (plugged == BatteryManager.BATTERY_PLUGGED_USB) powerStatus!!.text =
+                "Plugged to USB" else if (plugged == BatteryManager.BATTERY_PLUGGED_WIRELESS) powerStatus!!.text =
+                "Plugged to Wireless Charger" else powerStatus!!.text = "Not plugged"
+        }
+    }
+
+
+    private var batteryTxt: TextView? = null
+
+    private val mBatInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(ctxt: Context, intent: Intent) {
+            val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+            batteryTxt!!.text = "$level%"
+        }
+    }
+}
